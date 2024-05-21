4b1d6a0bab64ccfae521f8008fc4c028d1f10bd5
RobsonFe <robson_20ferreira@hotmail.com>
2024-05-21 20:19:14 -0300

    fix:buscarPorNome

--- src/main/java/io/github/robsonfe/rentservice/service/GerenciadorLocacoes.java
+++ src/main/java/io/github/robsonfe/rentservice/service/GerenciadorLocacoes.java
@@ -73,10 +73,12 @@ public class GerenciadorLocacoes {
     }
 
     @Transactional(readOnly = true)
-    public List<Cliente> buscarPorNome(String nome){
-        String jpql = " select c from tb_cliente c where c.nome like :nome ";
+    public List<Cliente> buscarPorNome(String nome, int page, int size) {
+        String jpql = "SELECT c FROM Cliente c WHERE c.name LIKE :nome";
         TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
         query.setParameter("nome", "%" + nome + "%");
+        query.setFirstResult(page * size);
+        query.setMaxResults(size);
         return query.getResultList();
     }
 }
