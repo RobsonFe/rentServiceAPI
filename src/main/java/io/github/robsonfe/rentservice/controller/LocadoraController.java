0a37c6cf54c0eb3cef7634b34300c8af2e5ade7e
RobsonFe <robson_20ferreira@hotmail.com>
2024-05-22 01:20:42 -0300

    update:DocForLocacaoController

--- src/main/java/io/github/robsonfe/rentservice/controller/LocadoraController.java
+++ src/main/java/io/github/robsonfe/rentservice/controller/LocadoraController.java
@@ -3,6 +3,11 @@ package io.github.robsonfe.rentservice.controller;
 import io.github.robsonfe.rentservice.model.Cliente;
 import io.github.robsonfe.rentservice.model.Locacao;
 import io.github.robsonfe.rentservice.service.GerenciadorLocacoes;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Tag(name = "Locação", description = "Contém todos os recursos referentes ao registro de locação")
 @RestController
 @RequestMapping("api/v1/locacoes")
 public class LocadoraController {
@@ -17,7 +23,18 @@ public class LocadoraController {
     @Autowired
     private GerenciadorLocacoes gerenciadorLocacoes;
 
-
+    @Operation(summary = "Cadastrar uma Locação",
+            description = "Você cadastra uma locação com data inicial e data final dessa locação",
+            responses = {
+            @ApiResponse(responseCode = "200", description = "Cadastrado com suceso!",
+                    content = @Content(mediaType = "application/json", schema = @Schema(
+                          implementation = LocadoraController.class
+                    ))),
+                    @ApiResponse(responseCode = "400", description = "A data inicial não pode ser maior do que a data final",
+                            content = @Content(mediaType = "application/json", schema = @Schema(
+                                    implementation = Error.class
+                            )))
+            })
     @PostMapping
     public ResponseEntity<Locacao> cadastrarLocacao(
             @RequestParam Long clienteId,
@@ -34,11 +51,39 @@ public class LocadoraController {
 
     }
 
+    @Operation(summary = "Listar Todas as Locações",
+            description = "Listagem de todas as locações",
+            responses = {
+            @ApiResponse(responseCode = "200", description = "Listagem Concluída!",
+                    content = @Content(mediaType = "application/json", schema = @Schema(
+                            implementation = LocadoraController.class
+                    ))),
+                    @ApiResponse(responseCode = "202", description = "Não possui lista a ser exibida",
+                            content = @Content(mediaType = "application/json", schema = @Schema(
+                                    implementation = Error.class
+                            ))),
+                    @ApiResponse(responseCode = "404", description = "Não foi encontrado nenhum dado no servidor.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(
+                                    implementation = Error.class
+                            )))
+            })
     @GetMapping
     public List<Locacao> listarLocacoes(){
         return gerenciadorLocacoes.listarLocacoes();
     }
 
+    @Operation(summary = "Consultar Locação",
+            description = "Buscar por locações conforme ID fornecido",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Busca Concluída!",
+                            content = @Content(mediaType = "application/json", schema = @Schema(
+                                    implementation = LocadoraController.class
+                            ))),
+                    @ApiResponse(responseCode = "202", description = "Não possui Locação",
+                            content = @Content(mediaType = "application/json", schema = @Schema(
+                                    implementation = Error.class
+                            )))
+            })
     @GetMapping("consultar/{id}")
     public ResponseEntity<Locacao> consultarLocacao(@PathVariable Long id){
         try {
@@ -49,11 +94,29 @@ public class LocadoraController {
         }
     }
 
+    @Operation(summary = "Buscar pelo Nome do Cliente",
+            description = "Resultado de busca pelo o nome do cliente conforme nome fornecido",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Nome do usuario encontrado",
+                            content = @Content(mediaType = "application/json", schema = @Schema(
+                                    implementation = LocadoraController.class
+                            ))),
+                    @ApiResponse(responseCode = "202", description = "Não possui esse nome na lista",
+                            content = @Content(mediaType = "application/json", schema = @Schema(
+                                    implementation = Error.class
+                            )))
+            })
     @GetMapping("buscar/nome")
     public List<Cliente> buscarPorNome(@RequestParam String nome){
         return  gerenciadorLocacoes.buscarPorNome(nome);
     }
 
+    @Operation(summary = "Cancelar Locação",
+            description = "Cancela uma locação com base no ID fornecido",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Locação cancelada com sucesso!"),
+                    @ApiResponse(responseCode = "404", description = "Locação não encontrada")
+            })
     @DeleteMapping("cancelar/{id}")
     public ResponseEntity<Void> cancelarLocacao(@PathVariable Long id){
 
